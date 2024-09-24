39d9cacfe8a384c34f6be33f5c2d0bf27f69a375
skar777123 <163728641+skar777123@users.noreply.github.com>
2024-09-24 11:02:25 +0530

    Update userRoute.js

--- userRoute.js
+++ userRoute.js
@@ -1,4 +1,4 @@
-import { Data, fetch, login, register } from "./userController.js";
+import { Data, deleteUser, fetch, login, register } from "./userController.js";
 import express from "express";
 const router = express.Router();
 
@@ -6,5 +6,6 @@ router.post("/Scholorship", Data);
 router.get("/fetch", fetch);
 router.post("/Admin-login", login);
 router.post("/Admin-register", register);
+router.post("/delete", deleteUser);
 
 export default router;
