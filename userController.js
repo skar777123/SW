86a081043129252af283afcdfa7d109b214cd8b9
skar777123 <sanskargaikwad206@gmail.com>
2024-11-15 18:59:33 +0530

    "Updated .env file, added json2csv dependency, modified userController.js and userRoute.js files"

--- userController.js
+++ userController.js
@@ -1,5 +1,9 @@
 import User from "./userModel.js";
 import Admin from "./AdminModel.js";
+import { Parser } from "json2csv";
+import twilio from "twilio";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const Data = async (req, res) => {
   const {
@@ -165,6 +169,83 @@ export const deleteUser = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "User deleted",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const File = async (req, res) => {
+  try {
+    const users = await User.find();
+    console.log(users);
+    const fields = [
+      "name",
+      "program",
+      "DOB",
+      "courseFee",
+      "feeReceipt",
+      "studentId",
+      "previousMarks",
+      "curricular",
+      "mobile",
+      "email",
+      "address",
+      "aadhar",
+      "caste",
+      "fatherOcc",
+      "motherOcc",
+      "Income",
+      "IncomeUpload",
+      "OtherFoS",
+      "OtherFoSyes",
+      "financeAssist",
+      "bankName",
+      "bankAccNo",
+      "bankIFSC",
+      "bankBranch",
+      "bankUpload",
+      "ReHOD",
+      "ReDoP",
+      "attendance1",
+      "attendance2",
+      "eleBill",
+    ];
+
+    const parser = new Parser({ fields });
+    const csv = await parser.parse(users);
+    res
+      .header(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      )
+      .attachment("Scholarship-submited-data.csv")
+      .status(200)
+      .send(csv);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const Approve = async (req, res) => {
+  const number = req.body.number;
+  const accountSid = process.env.SID;
+  const authToken = process.env.TK;
+  try {
+    const client = twilio(accountSid, authToken);
+    client.messages.create({
+      body: `B.K.Birla College,Kalyan               Your Scholarship Application has been Approved.We will soon Notify the Further Process`,
+      from: process.env.NUM,
+      to: "+91" + number,
+    });
+    res.status(200).json({
+      success: true,
     });
   } catch (error) {
     res.status(400).json({
