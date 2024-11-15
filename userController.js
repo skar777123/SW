79f51b3873d2ec8ee47e4aacb02fb5354a134a53
Sanskar_Gaikwad <163728641+skar777123@users.noreply.github.com>
2024-11-15 20:26:19 +0530

    Update userController.js

--- userController.js
+++ userController.js
@@ -1,10 +1,15 @@
 import User from "./userModel.js";
 import Admin from "./AdminModel.js";
+import { Parser } from "json2csv";
+import twilio from "twilio";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const Data = async (req, res) => {
   const {
     name,
-    program,
+    department,
+    course,
     courseFee,
     DOB,
     feeReceipt,
@@ -29,18 +34,16 @@ export const Data = async (req, res) => {
     bankBranch,
     bankUpload,
     ReHOD,
-    ReVP,
     ReDoP,
     attendance1,
     attendance2,
     eleBill,
-    photo,
-    sign
   } = req.body;
   try {
     const user = await User.create({
       name,
-      program,
+      department,
+      course,
       courseFee,
       DOB,
       feeReceipt,
@@ -65,13 +68,10 @@ export const Data = async (req, res) => {
       bankBranch,
       bankUpload,
       ReHOD,
-      ReVP,
       ReDoP,
       attendance1,
       attendance2,
       eleBill,
-      photo,
-    sign
     });
     res
       .header(
@@ -123,7 +123,7 @@ export const login = async (req, res) => {
       res
         .header(
           "Access-Control-Allow-Origin",
-          "https://scholarship-form-birla-4vuq.vercel.app"
+          "https://scholarship-form-birla-4vuq.vercel.app/"
         )
         .status(200)
         .json({ success: true, message: "User logged in successfully" });
@@ -165,10 +165,88 @@ export const register = async (req, res) => {
 export const deleteUser = async (req, res) => {
   const { id } = req.body;
   try {
-    const User = await User.findByIdAndDelete({ _id: id });
+    const user = await User.findByIdAndDelete({ _id: id });
     res.status(200).json({
       success: true,
       message: "User deleted",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const File = async (req, res) => {
+  try {
+    const users = await User.find();
+    console.log(users);
+    const fields = [
+      "name",
+      "program",
+      "DOB",
+      "courseFee",
+      "feeReceipt",
+      "studentId",
+      "previousMarks",
+      "curricular",
+      "mobile",
+      "email",
+      "address",
+      "aadhar",
+      "caste",
+      "fatherOcc",
+      "motherOcc",
+      "Income",
+      "IncomeUpload",
+      "OtherFoS",
+      "OtherFoSyes",
+      "financeAssist",
+      "bankName",
+      "bankAccNo",
+      "bankIFSC",
+      "bankBranch",
+      "bankUpload",
+      "ReHOD",
+      "ReDoP",
+      "attendance1",
+      "attendance2",
+      "eleBill",
+    ];
+
+    const parser = new Parser({ fields });
+    const csv = await parser.parse(users);
+    res
+      .header(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      )
+      .attachment("Scholarship-submited-data.csv")
+      .status(200)
+      .send(csv);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+export const Approve = async (req, res) => {
+  const number = req.body.number;
+  const accountSid = process.env.SID;
+  const authToken = process.env.TK;
+  try {
+    const client = twilio(accountSid, authToken);
+    client.messages.create({
+      body: `B.K.Birla College,Kalyan               Your Scholarship Application has been Approved.We will soon Notify the Further Process`,
+      from: process.env.NUM,
+      to: "+91" + number,
+    });
+    res.status(200).json({
+      success: true,
     });
   } catch (error) {
     res.status(400).json({
